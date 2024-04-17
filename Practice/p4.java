6f162e0a8866f06002f174929da9af378d064b6a
정재운 <81712500+UN15@users.noreply.github.com>
2024-04-17 23:22:57 +0900

    Create p4.java

---
+++ Practice/p4.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class p4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		
+//		char[] c = a.toCharArray();
+//		for(char ch: c) {
+//			if(Character.isUpperCase(ch)) {
+//				ch = Character.toLowerCase(ch);
+//			}
+//			else {
+//				ch = Character.toUpperCase(ch);
+//			}
+//			System.out.println(ch);
+//
+//		}
+//		for(int i = 0; i<a.length(); i++) {
+//			char c  = a.charAt(i);
+//			if(Character.isUpperCase(c)) {
+//				c = Character.toLowerCase(c);
+//			}
+//			else {
+//				c = Character.toUpperCase(c);
+//			}
+//		}
+	}
+}
