6993ea57e56b2d307de0f62ab9785ef14b392853
정재운 <81712500+UN15@users.noreply.github.com>
2024-04-22 22:28:05 +0900

    Update p17.java

--- Practice2/p17.java
+++ Practice2/p17.java
@@ -12,12 +12,14 @@ public class p17 {
 	}
 	public static int solution(int a, int b, int c) {
 		int answer = 0;
-		if(a == b and b == c) {
-			
+		if(a == b && b == c) {
+			answer += ((a+b+c)*(Math.pow(a, 2)+Math.pow(b, 2)+Math.pow(c, 2))*(Math.pow(a, 3)+Math.pow(b, 3)+Math.pow(c, 3)));
 		}
-		else if(a!=b and b!=c) {
-			
+		else if(a!=b && b!=c && a != c) {
+			answer += (a+b+c);
 		}
+		else
+			answer += ((a+b+c)*(Math.pow(a, 2)+Math.pow(b, 2)+Math.pow(c, 2)));
 		return answer;
 	}
 }
