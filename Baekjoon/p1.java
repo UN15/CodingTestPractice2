c555f95c95990cf4f4fbb0cf21ca0dbac085cce9
정재운 <81712500+UN15@users.noreply.github.com>
2024-04-23 23:10:27 +0900

    Create p1.java

---
+++ Baekjoon/p1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class p1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if(a>b)
+			System.out.println(">");
+		else if(a<b)
+			System.out.println("<");
+		else if (a == b)
+			System.out.println("==");
+		
+	}
+
+}
