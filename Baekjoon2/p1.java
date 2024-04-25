1ccf1727517b99db3750c45678a5ad71838ba97c
정재운 <81712500+UN15@users.noreply.github.com>
2024-04-25 23:34:06 +0900

    baekjoon2 진행중

---
+++ Baekjoon2/p1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class p1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		ArrayList
+		for(int i = 0; i<n; i++)
+			sc.nextInt();
+	}
+
+}
