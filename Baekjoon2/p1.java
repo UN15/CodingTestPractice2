a55b525daed16cb20630c7b6b83b6276716621ad
정재운 <81712500+UN15@users.noreply.github.com>
2024-04-26 23:19:49 +0900

    Update p1.java

--- Baekjoon2/p1.java
+++ Baekjoon2/p1.java
@@ -4,10 +4,18 @@ public class p1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		ArrayList
+		int n, v;
+		int count = 0;
+		n = sc.nextInt();
+		int[] arr = new int[n];
 		for(int i = 0; i<n; i++)
-			sc.nextInt();
+			arr[i] = sc.nextInt();
+		v = sc.nextInt();
+		for(int i = 0; i<n; i++)
+			if(arr[i] == v)
+				count++;
+		System.out.println(count);
+		
 	}
 
 }
