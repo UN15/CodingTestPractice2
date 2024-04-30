48d247a7128d302446a41f689b0ecf15f801e59e
정재운 <81712500+UN15@users.noreply.github.com>
2024-04-30 23:35:34 +0900

    p10 진행중

---
+++ Baekjoon2/p10.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class p10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i<n; i++)
+			arr[i] = sc.nextInt();
+		
+	}
+
+}
