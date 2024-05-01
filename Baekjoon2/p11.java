d12efee75b21b6686065efb3eaec6b7022390a2d
정재운 <81712500+UN15@users.noreply.github.com>
2024-05-01 22:27:04 +0900

    문자열 시작

---
+++ Baekjoon2/p11.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class p11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int i = sc.nextInt();
+		System.out.println(str.charAt(i-1));
+	}
+
+}
---
+++ Baekjoon2/p12.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class p12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(str.length());
+	}
+
+}
