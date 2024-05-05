aaec17b49b9ec788b3703f068b4ca540acc890f4
정재운 <81712500+UN15@users.noreply.github.com>
2024-05-05 17:53:13 +0900

    별 찍기 완료

--- Baekjoon3/p2.java
+++ Baekjoon3/p2.java
@@ -5,7 +5,28 @@ public class p2 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		int i, j;
+		int l;
+		int k = n;
+		for(i = n-1; i>=0; i--) {
+			for(j = 0; j<i; j++)
+				System.out.print(" ");
+			for(l = k-i; l>0; l--)
+				System.out.print("*");
+			System.out.println();
+			k++;
+		}
+		k-=2;
+		for(i = 1; i<n; i++) {
+			for(j = 0; j<i; j++)
+				System.out.print(" ");
+			for(l = i; l<k; l++)
+				System.out.print("*");
+			System.out.println();
+			k--;
+		}
+		
 		
 	}
-
 }
+
