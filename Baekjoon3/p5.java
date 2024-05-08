c5a18459a56d7617433038c1b17105f1efb03941
정재운 <81712500+UN15@users.noreply.github.com>
2024-05-08 20:28:03 +0900

    Update p5.java

--- Baekjoon3/p5.java
+++ Baekjoon3/p5.java
@@ -6,36 +6,61 @@ public class p5 {
 		Scanner sc = new Scanner(System.in);
 		char[] c = sc.next().toCharArray();
 		int count = 0;
-		int i = 0;
-		while(i<c.length) {
-			switch(c[i]) {
-				case 'c':
-					if(i+1 < c.length) {
-						if(c[i+1] == '=')
-							count++;
-						else if(c[i+1] == '-')
-							count++;
-						i++;
-					}
-					else
-						count++;
-					break;
-				case 'd':
-					if(i+1 < c.length && c[i+1] == '-') {
-						count++;
-						i++;
-					}
-					else if(i+2<c.length && c[i+1] == 'z') {
-						if(c[i+2] == '=') {
-							count++;
-						}
-						else
-							count+=3;
-					}
-			i++;
+		int len = c.length;
+		for(int i = 0; i<len; i++) {		
+			if(c[i] == 'c' && i<len-1) {
+				if(c[i+1] == '=' || c[i+1] == '-') 
+					i++;
 			}
+			else if(c[i] == 'd' && i<len-1) {
+				if(c[i+1] == '-') 
+					i++;
+				
+				else if(c[i+1] == 'z' && i<len-2) {
+					if(c[i+2] == '=') {
+						i+=2;
+					}
+				}
+			}
+			
+			else if(c[i] == 'l' && i<len-1) {
+				if(c[i+1] == 'j')
+					i++;
+			}
+			else if(c[i] == 'n' && i<len-1) {
+				if(c[i+1] == 'j')
+					i++;
+			}
+			else if(c[i] == 's' && i<len-1) {
+				if(c[i+1] == '=')
+					i++;
+			}
+			else if(c[i] == 'z' && i<len-1) {
+				if(c[i+1] == '=')
+					i++;
+			}
+			count++;
 		}
 			
+		System.out.println(count);	
+		
 	}
-
 }
+
+
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String[] croatias = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+//        String str = sc.next();
+//        
+//        for (int i = 0; i < croatias.length; i++) {
+//            if (str.contains(croatias[i]))
+//                str = str.replace(croatias[i], "@");
+//        }
+//        System.out.println(str.length());
+//        sc.close();
+//    }
+//}
