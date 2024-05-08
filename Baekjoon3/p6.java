1d741571f748dced88119f68cec553cb2f9c78df
정재운 <81712500+UN15@users.noreply.github.com>
2024-05-08 20:29:18 +0900

    심화1 진행중

---
+++ Baekjoon3/p6.java
@@ -0,0 +1,21 @@
+import java.util.*;
+//그룹 단어 체커
+public class p6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		char c[];
+		for(int i = 0; i<n; i++) {
+			String str = sc.next();
+			c = new char[str.length()];
+			for(int j = 0; j<str.length(); j++)
+				if (str.charAt(j) == 0) {
+					
+				}
+		}
+			
+	}
+
+}
