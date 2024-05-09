6f52ee96ea62bca55a64f4ec3077f3847548afea
정재운 <81712500+UN15@users.noreply.github.com>
2024-05-09 18:09:12 +0900

    Update p8.java

--- Baekjoon3/p8.java
+++ Baekjoon3/p8.java
@@ -6,11 +6,20 @@ public class p8 {
 		Scanner sc = new Scanner(System.in);
 		int[][] a = new int[9][9];
 		int max = -1;
-		int max_i;
-		int max_j;
-		for(int i = 0; i<9; i++)
-			for(int j = 0; j<9; j++)
+		int max_i = 0, max_j = 0;
+		for(int i = 0; i<9; i++) {
+			for(int j = 0; j<9; j++) {
 				a[i][j] = sc.nextInt();
+				if(a[i][j]>max) {
+					max = a[i][j];
+					max_i = i+1;
+					max_j = j+1;
+				}
+			}
+		}
+		
+		System.out.println(max);
+		System.out.print(max_i+" "+max_j);
 	}
 
 }
