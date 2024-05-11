42ff058fe8ab9b805cb1db09988859086171a5d2
정재운 <81712500+UN15@users.noreply.github.com>
2024-05-11 15:52:42 +0900

    Update p3.java

--- Baekjoon4/p3.java
+++ Baekjoon4/p3.java
@@ -7,7 +7,22 @@ public class p3 {
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br. readLine());
+		int t = Integer.parseInt(br.readLine());
+		for(int i = 0; i<t; i++) {
+			int cost = Integer.parseInt(br.readLine());
+			
+			System.out.print(cost/25+" ");
+			cost = cost%25;
+
+			System.out.print(cost/10+" ");
+			cost = cost%10;
+
+			System.out.print(cost/5+" ");
+			cost = cost%5;
+
+			System.out.println(cost/1);
+			
+		}
 		
 	}
 
