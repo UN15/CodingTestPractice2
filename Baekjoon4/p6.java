67aacebaeae2503070bb05a8b5b545749f7abb0a
정재운 <81712500+UN15@users.noreply.github.com>
2024-05-12 23:11:11 +0900

    일반수학 진행중

---
+++ Baekjoon4/p6.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class p6 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int x = Integer.parseInt(br.readLine());
+		int a=1, b=1;
+		int count = 0;
+		while(count<x) {
+			count+=a;
+			a++;
+			
+			if(count%2 == 0) {
+				
+			}
+				
+			
+		}
+		
+	}
+
+}
