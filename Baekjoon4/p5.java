c9d68ae7974af77bb1ccd27b220508324599d671
정재운 <81712500+UN15@users.noreply.github.com>
2024-05-12 23:10:44 +0900

    Update p5.java

--- Baekjoon4/p5.java
+++ Baekjoon4/p5.java
@@ -5,7 +5,19 @@ public class p5 {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		
+		int a = 1;
+		int i = 1;
+		int count = 1;
+		while(a<n) {
+			a = a+6*i;
+			count++;
+			i++;
+		}
+		System.out.println(count);
 	}
 
 }
+
+// 벌집
+//한 마름모 마지막 1=>7=>19=>37=>61 즉 6 12 18 24 간격 6의 배수로 진행
+		
