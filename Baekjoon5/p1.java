c35b6fe53afb7c01e6f5dbb135c79ded4c1af451
정재운 <81712500+UN15@users.noreply.github.com>
2024-05-17 22:50:41 +0900

    Update p1.java

--- Baekjoon5/p1.java
+++ Baekjoon5/p1.java
@@ -1,27 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+//import java.util.*;
+//public class p1 {
+// 1. Scanner && System.out.println() 이용 방법
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		int m = sc.nextInt();
+//		while(n != 0 && m != 0) {
+//			if(m % n == 0)
+//				System.out.println("factor");
+//			else if(n%m == 0)
+//				System.out.println("multiple");
+//			else
+//				System.out.println("neither");
+//			n = sc.nextInt();
+//			m = sc.nextInt();
+//		}
+//
+//		
+//	}
+//}
+
+import java.io.*;
 import java.util.StringTokenizer;
 public class p1 {
-
-	public static void main(String[] args) throws IOException {
+//2.BufferedReader && StringBuilder 이용 방법
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] a = br.readLine().split(" ");
-		while(a[0] != "0" && a[1] != "0") {
-			int n = Integer.parseInt(a[0]);
-			int m = Integer.parseInt(a[1]);
-			
-			if(m%n == 0) 
-				System.out.println("factor");
-			else if(n%m == 0)
-				System.out.println("multiple");
-			else
-				System.out.println("neither");
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 
-			 a = br.readLine().split(" ");
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			if(n == 0 && m == 0)
+				break;
+			if(m % n == 0)
+				sb.append("factor\n");
+			else if(n%m == 0)
+				sb.append("multiple\n");			
+			else
+				sb.append("neither\n");
 
 		}
+		System.out.println(sb);
+		
 	}
-
 }
