9aa9fa04df10416fbfd05b610412817076245038
정재운 <81712500+UN15@users.noreply.github.com>
2024-05-19 21:52:06 +0900

    Update p8.java

--- Baekjoon5/p8.java
+++ Baekjoon5/p8.java
@@ -1,14 +1,19 @@
 import java.io.*;
-import java.util.StringTokenizer;
 public class p8 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] sarr = br.readLine().split(" ");
-		int min = -1;
-		for(String s: sarr) {
-			min = Math.min(min, Integer.parseInt(s));
+		int x = Integer.parseInt(sarr[0]);
+		int y = Integer.parseInt(sarr[1]);
+		int w = Integer.parseInt(sarr[2]);
+		int h = Integer.parseInt(sarr[3]);
+		int[] carr = {x, y, w-x, h-y};
+		int min = Integer.MAX_VALUE;
+		
+		for(int c: carr) {
+			min = Math.min(min, c);
 		}
 		System.out.println(min);
 	}
