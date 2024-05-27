19c2eec3b258e0b555827a323409a1e9fa5364b0
정재운 <81712500+UN15@users.noreply.github.com>
2024-05-27 21:15:58 +0900

    Update p2.java

--- Sort/p2.java
+++ Sort/p2.java
@@ -1,1 +1,23 @@
-import java.io.*
+public class p2 {
+// 2. 선택 정렬(Selection Sort)
+//minIndex를 찾고 교환
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {2, 6, 7, 9, 1, 3, 5, 4, 8};
+		for(int i = 0; i<arr.length-1; i++) {
+			int minIdx = i;
+			for(int j = i+1; j<arr.length; j++) {
+				if(arr[j]<arr[minIdx]) {
+					minIdx = j;
+				}
+			}
+			swap(arr, i, minIdx);
+		}
+	}
+	public static void swap(int[] arr, int idx1, int idx2) {
+		int tmp = arr[idx1];
+		arr[idx1] = arr[idx2];
+		arr[idx2] = tmp;
+	}
+
+}
