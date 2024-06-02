f3cb89481df7852e9f680669a15ae0a5f63832ee
정재운 <81712500+UN15@users.noreply.github.com>
2024-06-03 00:38:13 +0900

    Update p4.java

--- Sort/p4.java
+++ Sort/p4.java
@@ -2,7 +2,19 @@ public class p4 {
 	// 4. 셸 정렬(Shell Sort)
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int[] arr = {2, 6, 7, 9, 1, 3, 5, 4, 8};
+		for(int h = arr.length/2; h>0; h/=2) {
+			for(int i = h; i<arr.length; i++) {
+				int tmp = arr[i];
+				int j = i-h;
+				while(j>=0 && arr[j]>tmp) {
+					arr[j+h] = arr[j];
+					j-=h;
+				}
+				arr[j+h] = tmp;
+			}
+		}
+			
 
 	}
 
