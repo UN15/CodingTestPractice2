125b8cbb311aed9cb878bd4c0208a90c76eb39a0
정재운 <81712500+UN15@users.noreply.github.com>
2024-06-07 23:42:55 +0900

    Update p5.java

--- Baekjoon6/p5.java
+++ Baekjoon6/p5.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.Arrays;
 public class p5 {
-
+// O(n^2) => 코드 수정 필요
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
