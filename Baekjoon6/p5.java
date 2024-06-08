3b0a7bf771ac5005cd8c8f57c1334aee77367703
정재운 <81712500+UN15@users.noreply.github.com>
2024-06-08 22:37:28 +0900

    p5 완료

--- Baekjoon6/p5.java
+++ Baekjoon6/p5.java
@@ -1,20 +1,29 @@
 import java.io.*;
-import java.util.Arrays;
+//10989
 public class p5 {
-// O(n^2) => 코드 수정 필요
-	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		for(int i = 0; i<n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		Arrays.sort(arr);
-		for(int i = 0; i<n; i++)
-			sb.append(arr[i]+"\n");
-		System.out.print(sb);
-	}
 
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine()); //입력 숫자의 개수
+
+        int[]arrays = new int[10001]; //N이 10,000보다 작거나 같다고 했으므로 인덱스가 0부터이므로 10001까지
+
+        for(int i = 0; i < N; i++){
+            arrays[Integer.parseInt(br.readLine())]++; //해당 입력받은 값을 인덱스로 갖는 요소의 값을 증가시킴
+        }
+        
+        br.close();
+
+        for(int i = 0; i < 10001; i++){
+            while(arrays[i] > 0){ //0보다 크면 ++한것이므로 입력받은 숫자에 속한다는 뜻
+                sb.append(i + "\n"); //arrays[i] 값은 i의 값이 몇번있냐를 의미하는 것이고, 실질적으로 입력받은 값은 i이다.
+                arrays[i]--; //한번 꺼냈으니 --해준다.
+            }
+        }
+
+        System.out.println(sb);
+
+    }
 }
