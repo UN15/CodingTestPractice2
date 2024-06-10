fdfd63ad5c720dd761bc978b3c7cac9451cff7f5
정재운 <81712500+UN15@users.noreply.github.com>
2024-06-10 23:19:11 +0900

    p7 완료

--- Baekjoon6/p7.java
+++ Baekjoon6/p7.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 public class p7 {
 
@@ -13,8 +14,15 @@ public class p7 {
 			st = new StringTokenizer(br.readLine());
 			arr[i][0] = Integer.parseInt(st.nextToken());
 			arr[i][1] = Integer.parseInt(st.nextToken());
-
 		}
+		Arrays.sort(arr, (num1, num2)->{
+			return num1[0] != num2[0] ? num1[0]-num2[0]: num1[1]- num2[1];
+		});
+		
+		for(int i = 0; i<n; i++) {
+			sb.append(arr[i][0]+ " "+ arr[i][1]);
+		}
+		System.out.println(sb);
 	}
 
 }
