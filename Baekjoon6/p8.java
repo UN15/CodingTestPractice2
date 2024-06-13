f7d0ea8008a0463d16938f3bc31b841cece60dae
정재운 <81712500+UN15@users.noreply.github.com>
2024-06-13 23:49:29 +0900

    Update p8.java

--- Baekjoon6/p8.java
+++ Baekjoon6/p8.java
@@ -17,11 +17,17 @@ public class p8 {
 			arr[i][0] = Integer.parseInt(st.nextToken());
 			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(str, new Comparator<Integer>() {
-//			public int compare(Integer i1, Integer i2) {
-//				
-
-			
+		//Arrays.sort(arr, new Comparator<Integer[]>() {
+	//		@Override
+	//		public int compare(Integer[] o1, Integer[] o2) {
+	//			if(o1[1].equals(o2[1])) {
+	//				return o1[0] - o2[0];
+	//			}
+	//			else {
+	//				return o1[1] - o2[1];
+	//			}	
+	//		}
+		//	});
 		Arrays.sort(arr, (num1, num2)->{
 			return num1[1] != num2[1] ? num1[1]- num2[1]: num1[0]- num2[0];
 		});
