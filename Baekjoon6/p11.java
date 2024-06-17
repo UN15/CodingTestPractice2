a6f240a55cd98f848e29be9778ecc6a0df83788d
정재운 <81712500+UN15@users.noreply.github.com>
2024-06-17 23:23:56 +0900

    Update p11.java

--- Baekjoon6/p11.java
+++ Baekjoon6/p11.java
@@ -1,30 +1,42 @@
-import java.io.*;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.Scanner;
+ 
 public class p11 {
-
-	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		int[] index = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for(int i = 0; i<n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			index[i] = arr[i];
-		}
-		
-		Arrays.sort(index);
-		
-		for(int i = 0; i<n; i++) {
-			int findIndex = Arrays.binarySearch(index, arr[i]);
-			sb.append(findIndex+" ");
-		}
-		
-		System.out.println(sb);
-	 }
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+ 
+        int[] array = new int[n];//원본 배열
+        int[] arraySort = new int[n];//정렬된 배열
+        HashMap<Integer, Integer> rankMap = new HashMap<Integer, Integer>();//압축할 map
+ 
+        //배열 입력
+        for(int i=0; i<n; i++){
+            array[i] = arraySort[i] = sc.nextInt();
+        }
+ 
+        //배열 정렬
+        Arrays.sort(arraySort);
+ 
+ 
+        //배열의 압축값을 map에 저장
+        int rank = 0;
+ 
+        for(int value : arraySort){
+            if(!rankMap.containsKey(value)){
+                rankMap.put(value,rank);
+                rank++;
+            }
+        }
+ 
+        //원본 배열의 순서대로 해당 압축 배열을 출력
+        StringBuilder sb = new StringBuilder();
+        for(int value : array){
+            sb.append(rankMap.get(value)).append(' ');
+        }
+ 
+        System.out.println(sb);
+    }
 }
