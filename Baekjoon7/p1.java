d936ab71bb806bd8a2e31adb6a7b9ddb4273da06
정재운 <81712500+UN15@users.noreply.github.com>
2024-06-19 18:08:44 +0900

    Update p1.java

--- Baekjoon7/p1.java
+++ Baekjoon7/p1.java
@@ -1,25 +1,48 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 public class p1 {
-
+//10815
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st;
-
-		int[] arr = new int[n];
+		StringBuilder sb = new StringBuilder();
+		HashMap<String, Integer> hmap = new HashMap<>();
 		st = new StringTokenizer(br.readLine(), " ");
 		for(int i = 0; i<n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			hmap.put(st.nextToken(), 0);
 		}
 		
 		int m = Integer.parseInt(br.readLine());
-		int[] arr2 = new int[m];
 		st = new StringTokenizer(br.readLine(), " ");
+//		for(int i = 0; i<m; i++) {
+//			String a = st.nextToken();
+//			if(hmap.containsKey(a)) {
+//				sb.append(1+" ");
+//			}
+//			else {
+//				sb.append(0+" ");
+//		
+//			}
+//		}
+//		
+//		System.out.println(sb);
 		for(int i = 0; i<m; i++) {
-			arr2[i] = Integer.parseInt(st.nextToken());
+			String a = st.nextToken();
+			if(hmap.get(a) != null) {
+				sb.append(1+" ");
+			}
+			else {
+				sb.append(0+" ");
+		
+			}
 		}
+		
+		System.out.println(sb);
 	}
 
 }
