7143a9b4ac78e84bab0ae4f1e4b4e5d007f33990
정재운 <81712500+UN15@users.noreply.github.com>
2024-06-21 23:07:26 +0900

    Create p4.java

---
+++ Baekjoon7/p4.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+public class p4 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		for(int i = 0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			String a = st.nextToken();
+		}
+			
+		System.out.println(sb);
+	}
+
+}
