4d12b9cc0d81ccc37081201beec029e1da847d87
정재운 <81712500+UN15@users.noreply.github.com>
2024-06-23 23:27:05 +0900

    Update p6.java

--- Baekjoon7/p6.java
+++ Baekjoon7/p6.java
@@ -1,9 +1,44 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 public class p6 {
 //1764
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		String str;
+		String check;
+		HashMap <String, Integer> hmap = new HashMap<>();
+		ArrayList<String> arr = new ArrayList<>();
+		for(int i = 0; i<n; i++) {
+			str = br.readLine();
+			hmap.put(str, 1);
+		}
+
+		for(int i = 0; i<m; i++) {
+			check = br.readLine();
+			if(hmap.containsKey(check)) {
+				arr.add(check);
+			}
+		}
+		
+		Collections.sort(arr);
+		int count = 0;
+		for(String s: arr) {
+			count++;
+			sb.append(s+"\n");
+		}
+		
+		System.out.println(count);
+		System.out.println(sb);
 	}
 
 }
