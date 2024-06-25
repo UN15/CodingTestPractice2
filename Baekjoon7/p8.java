b982c857127290d9ff10348042f14f874350c2d6
정재운 <81712500+UN15@users.noreply.github.com>
2024-06-25 18:30:43 +0900

    Update p8.java

--- Baekjoon7/p8.java
+++ Baekjoon7/p8.java
@@ -1,20 +1,25 @@
-import java.io.*;
-import java.util.HashMap;
-public class p8 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.io.IOException;
 //11478
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String s = br.readLine();
-		HashMap <String, Integer> hmap = new HashMap<>();
-		while(true) {
-			for(int i = 1; i<s.length(); i++) {
-				for(int j = 0; j<=i; j++) {
-					s.substring(j, i);
-				}
-				hmap.put(,1);
-			}
-		}
-	}
+public class p8 {
 
+   public static void main(String[] args) throws IOException {
+
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      String str = br.readLine();
+      Set<String> set = new HashSet<>();
+
+      for (int i = 0; i < str.length(); i++) {
+         for (int j = i + 1; j <= str.length(); j++) {
+            set.add(str.substring(i, j));
+         }
+      }
+
+      System.out.println(set.size());
+      
+   }
 }
