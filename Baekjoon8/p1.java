9a67ec7c0be78bde8a483afe67c4fb02ee3784ca
정재운 <81712500+UN15@users.noreply.github.com>
2024-06-26 23:28:51 +0900

    Create p1.java

---
+++ Baekjoon8/p1.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+public class p1 {
+//1934
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i<t; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			while(true) {
+				for(int i = 2; i<8)
+			}
+			
+			sb.append();
+		}
+		
+		System.out.println(sb);
+		
+	}
+
+}
