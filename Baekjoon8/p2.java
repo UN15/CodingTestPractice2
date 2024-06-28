08f79ca90764d2c35a0fd333683311c3f8832cbb
정재운 <81712500+UN15@users.noreply.github.com>
2024-06-28 23:10:16 +0900

    p2 진행중

---
+++ Baekjoon8/p2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class p2 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		System.out.println(sb);
+	}
+
+}
