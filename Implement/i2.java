0fd84cbf03993f9aa7579e859fa3532f7e0a6cb1
정재운 <81712500+UN15@users.noreply.github.com>
2024-07-08 23:30:48 +0900

    Update i2.java

--- Implement/i2.java
+++ Implement/i2.java
@@ -5,15 +5,18 @@ public class i2 {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-
+		int count = 0;
 		for(int i = 1; i<=n; i++;){
 			for(int j = 0; j<60; j++;){
 				for(int k = 0; k<60; k++;){
-			           if 
-		                 }
-			
-		         }
+			           if (String.valueOf(i).contains("3") || String.valueOf(j).contains("3") || String.valueOf(k).contains("3")){
+				   	count++;
+				   }
+		               }
+			}
 		}
+
+		System.out.println(count);
 		
 	}
 
