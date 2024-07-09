b3b236e6af9d2f8d8026ad6ebee1c6e89e21c0ed
정재운 <81712500+UN15@users.noreply.github.com>
2024-07-09 23:32:47 +0900

    Create i3.java

---
+++ Implement/i3.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class i3 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[] arr = br.readLine().toCharArray();
+	}
+
+}
