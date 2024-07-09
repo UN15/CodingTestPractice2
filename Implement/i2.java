f9ed85c2f49500066f0419357ee0c5c358223d06
정재운 <81712500+UN15@users.noreply.github.com>
2024-07-09 23:32:23 +0900

    Update i2.java

--- Implement/i2.java
+++ Implement/i2.java
@@ -6,11 +6,11 @@ public class i2 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int count = 0;
-		for(int i = 1; i<=n; i++;){
-			for(int j = 0; j<60; j++;){
-				for(int k = 0; k<60; k++;){
+		for(int i = 0; i<=n; i++){
+			for(int j = 0; j<60; j++){
+				for(int k = 0; k<60; k++){
 			           if (String.valueOf(i).contains("3") || String.valueOf(j).contains("3") || String.valueOf(k).contains("3")){
-				   	count++;
+			        	   count++;
 				   }
 		               }
 			}
