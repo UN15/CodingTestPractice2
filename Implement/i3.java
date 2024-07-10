8d4663f79280e23e899f04b0b13423567933ebf7
정재운 <81712500+UN15@users.noreply.github.com>
2024-07-10 23:48:31 +0900

    Update i3.java

--- Implement/i3.java
+++ Implement/i3.java
@@ -5,6 +5,38 @@ public class i3 {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		char[] arr = br.readLine().toCharArray();
+		int x = arr[0]-'a';
+		int y = arr[1]-'0'-1;
+		System.out.println(x+" "+ y);
+		int count = 0;
+		//수평2 수직1
+		if(x+2<8 && y+1<8) {
+			count++;
+		}
+		if (x+2<8 && y-1>=0) {
+			count++;
+		}
+		if (x-2>=0 && y+1<8) {
+			count++;
+		}
+		if (x-2>=0 && y-1>=0) {
+			count++;
+		}
+		//수직 2 수평 1
+		if(x+1<8 && y+2<8) {
+			count++;
+		}
+		if (x+1<8 && y-2>=0) {
+			count++;
+		}
+		if (x-1>=0 && y+2<8) {
+			count++;
+		}
+		if (x-1>=0 && y-2>=0) {
+			count++;
+		}
+		System.out.println(count);
+		
 	}
 
 }
