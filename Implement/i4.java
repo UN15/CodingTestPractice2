7f6bb00de96baa1994470b0165e43e3bb409dc87
정재운 <81712500+UN15@users.noreply.github.com>
2024-07-12 23:50:17 +0900

    Update i4.java

--- Implement/i4.java
+++ Implement/i4.java
@@ -8,7 +8,18 @@ public class i4 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		
+		int[][] arr = new int[n][m];
+		for(int i = 0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j<m; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
 	}
-
 }
