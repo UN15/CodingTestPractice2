06ce21ddf843cecb69669cc120386bd28c760e35
정재운 <81712500+UN15@users.noreply.github.com>
2024-07-16 23:48:45 +0900

    Update i4.java

--- Implement/i4.java
+++ Implement/i4.java
@@ -1,3 +1,37 @@
+// import java.io.*;
+// import java.util.StringTokenizer;
+// public class i4 {
+
+// 	public static void main(String[] args) throws IOException {
+// 		// TODO Auto-generated method stub
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		StringTokenizer st = new StringTokenizer(br.readLine());
+// 		int n = Integer.parseInt(st.nextToken());
+// 		int m = Integer.parseInt(st.nextToken());
+// 		st = new StringTokenizer(br.readLine());
+// 		int a = Integer.parseInt(st.nextToken());
+// 		int b = Integer.parseInt(st.nextToken());
+// 		int d = Integer.parseInt(st.nextToken());
+// 		int[][] check = new int[n][m];
+// 		int[][] arr = new int[n][m];
+// 		for(int i = 0; i<n; i++) {
+// 			st = new StringTokenizer(br.readLine());
+// 			for(int j = 0; j<m; j++) {
+// 				arr[i][j] = Integer.parseInt(st.nextToken());
+// 				check[i][j] = 0;
+// 			}
+// 		}
+// 		check[a][b] = 1;
+
+// 		d-=1;
+// 		if(d<0){
+// 			d = 3;
+// 		}
+// 		if(d == 0)
+
+// 	}
+// }
+
 import java.io.*;
 import java.util.StringTokenizer;
 public class i4 {
@@ -12,22 +46,19 @@ public class i4 {
 		int a = Integer.parseInt(st.nextToken());
 		int b = Integer.parseInt(st.nextToken());
 		int d = Integer.parseInt(st.nextToken());
-		int[][] check = new int[n][m];
+		
 		int[][] arr = new int[n][m];
 		for(int i = 0; i<n; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j<m; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
-				check[i][j] = 0;
 			}
 		}
-		check[a][b] = 1;
-
-		d-=1;
-		if(d<0){
+		if(d-1<0)
 			d = 3;
-		}
-		if(d == 0)
+		
+		arr[a][b]
 
 	}
 }
+
