8b7cd853fa7d00605505d34e6f5136b3ce5f13c7
정재운 <81712500+UN15@users.noreply.github.com>
2024-07-17 23:49:52 +0900

    p4 진행중

---
+++ Baekjoon8/p4.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class p4 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		for(int i = 0; i<n; i++) {
+			br.readLine();
+		}
+	}
+
+}
