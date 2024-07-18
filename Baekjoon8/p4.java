0d144af7a0743377c43720816c85611428a74d28
정재운 <81712500+UN15@users.noreply.github.com>
2024-07-18 23:21:09 +0900

    Update p4.java

--- Baekjoon8/p4.java
+++ Baekjoon8/p4.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.StringTokenizer;
 public class p4 {
 
 	public static void main(String[] args) throws IOException{
@@ -6,7 +7,8 @@ public class p4 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		for(int i = 0; i<n; i++) {
-			br.readLine();
+			StringTokenizr st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
 		}
 	}
 
