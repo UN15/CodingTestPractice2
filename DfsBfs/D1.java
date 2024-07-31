aa13b4145c2d35a79930bf58ba888287533d4bf2
정재운 <81712500+UN15@users.noreply.github.com>
2024-08-01 00:02:03 +0900

    D1.java 완료

--- DfsBfs/D1.java
+++ DfsBfs/D1.java
@@ -9,19 +9,37 @@ public class D1 {
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		int[][] arr = new int[n][m];
+		int count = 0;
 		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
+			String str = br.readLine();
 			for(int j = 0; j<m; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-				check(arr, i, j);
+				arr[i][j] = str.charAt(j)-'0';		
 			}
 		}
+		
+		for(int i=0; i<n; i++) {
+			for(int j = 0; j<m; j++) {
+				boolean c = check(arr, i, j, n, m);
+				if(c) {
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
 	}
 	
-	static void check(int[][] arr, int i, int j) {
+	static boolean check(int[][] arr, int i, int j, int n, int m) {
+		if(i<0 || j<0 || i>=n || j>=m)
+			return false;
 		if(arr[i][j] == 0) {
-			
+			arr[i][j] = 1;
+			check(arr, i-1, j, n, m);
+			check(arr, i, j-1, n, m);
+			check(arr, i+1, j, n, m);
+			check(arr, i, j+1, n, m);
+			return true;
 		}
+		return false;
 	}
 
 }
