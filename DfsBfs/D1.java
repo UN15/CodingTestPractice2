c298cfe15d878c4ac7f15cf76da2b6bfe2f746fe
정재운 <81712500+UN15@users.noreply.github.com>
2024-07-31 23:04:40 +0900

    Update D1.java

--- DfsBfs/D1.java
+++ DfsBfs/D1.java
@@ -1,1 +1,27 @@
-import java.util.*;
+import java.io.*;
+import java.util.StringTokenizer;
+public class D1 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][] arr = new int[n][m];
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j<m; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				check(arr, i, j);
+			}
+		}
+	}
+	
+	static void check(int[][] arr, int i, int j) {
+		if(arr[i][j] == 0) {
+			
+		}
+	}
+
+}
