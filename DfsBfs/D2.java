a17db980d11bbae86f4fffe4eddd6226092bbe95
정재운 <81712500+UN15@users.noreply.github.com>
2024-08-02 23:11:54 +0900

    upload D2.java

---
+++ DfsBfs/D2.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+public class D2 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		char[][] arr = new char[n][m];
+		
+		for(int i = 0; i<n; i++) {
+			String str = br.readLine();
+			for(int j = 0; j<m; j++) {
+				arr[i][j] = str.charAt(j);
+			}
+		}
+		
+		check(arr, 0, 0);
+
+	}
+	
+	static void check(char[][] arr, int n, int m){
+		Queue<Integer> q = new LinkedList<Integer>();
+
+	}
+}
